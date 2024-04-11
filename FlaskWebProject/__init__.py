b580985935d834d3764a141ee42b6efca7c8b664
bach.tran <trannguyenbach1992@gmail.com>
2024-04-11 16:33:11 +0700

    add readme

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,9 +10,11 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://sa:!6Den10kytu@@192.168.49.1/bachtnsqlserver01?driver=ODBC+Driver+17+for+SQL+Server'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # TODO: Add any logging levels and handlers with app.logger
-logging.basicConfig(filename='app.log', level=logging.DEBUG,
-                    format='%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+# logging.basicConfig(filename='app.log', level=logging.DEBUG,
+#                     format='%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
 
 Session(app)
 db = SQLAlchemy(app)
--- config.py
+++ config.py
@@ -9,6 +9,12 @@ class Config(object):
     BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'ErB416CX+BvbbfiLZ9Nhm1FoitzPo4tJisdJDgw/s1ti3t+8g6OMfYiSCX68GOUljoxMpH4z/+al+ASt74L9Lg=='
     BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
 
+    # server = '192.168.49.1'  # e.g., 'localhost\sqlexpress' or IP address
+    # database = 'bachtnsqlserver01'
+    # username = 'sa'
+    # password = '!6Den10kytu@'
+    SQL_DRIVER = '{ODBC Driver 17 for SQL Server}' 
+
     SQL_SERVER = os.environ.get('SQL_SERVER') or '192.168.49.1'
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'bachtnsqlserver01' #'bachtn-sqlserver-01'
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'sa' #'bachtn'
@@ -16,6 +22,8 @@ class Config(object):
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SQL_CONN_STR = f'DRIVER={SQL_DRIVER};SERVER={SQL_SERVER};DATABASE={SQL_DATABASE};UID={SQL_USER_NAME};PWD={SQL_PASSWORD}'
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
@@ -39,28 +47,3 @@ class Config(object):
     SCOPE = ["User.Read"] # Only need to read user profile for this app
 
     SESSION_TYPE = "filesystem"  # Token cache will be stored in server-side session
-
-
-      
-
-    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app
-    # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
-
-    # TODO: Enter your application client ID here
-    CLIENT_ID = "eef84f2a-99f2-49ea-b886-ff525085c9b4"
-
-    # TODO: Enter the redirect path you want to use for OAuth requests
-    #   Note that this will be the end of the URI entered back in Azure AD
-    REDIRECT_PATH = "/callback"  # Used to form an absolute URL, 
-        # which must match your app's redirect_uri set in AAD
-
-    # You can find the proper permission names from this document
-    # https://docs.microsoft.com/en-us/graph/permissions-reference
-    SCOPE = ["User.Read"]
-
-
-    # server = '192.168.49.1'  # e.g., 'localhost\sqlexpress' or IP address
-    # database = 'bachtnsqlserver01'
-    # username = 'sa'
-    # password = '!6Den10kytu@'
-    # driver = '{ODBC Driver 17 for SQL Server}' 
